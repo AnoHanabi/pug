e48d916a1f624593e1340d853838c20c11122c3e
Hanabi <1213513671@qq.com>
2019-04-24 21:41:48 +0800

    pug done

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -117,31 +117,31 @@ exports.author_delete_get = function (req, res, next) {
 };
 
 // 由 POST 处理作者删除操作
-exports.author_delete_post = function(req, res, next) {
+exports.author_delete_post = function (req, res, next) {
 
   async.parallel({
-      author: function(callback) {
-        Author.findById(req.body.authorid).exec(callback)
-      },
-      authors_books: function(callback) {
-        Book.find({ 'author': req.body.authorid }).exec(callback)
-      },
-  }, function(err, results) {
-      if (err) { return next(err); }
-      // Success
-      if (results.authors_books.length > 0) {
-          // Author has books. Render in same way as for GET route.
-          res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books } );
-          return;
-      }
-      else {
-          // Author has no books. Delete object and redirect to the list of authors.
-          Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
-              if (err) { return next(err); }
-              // Success - go to author list
-              res.redirect('/catalog/authors')
-          })
-      }
+    author: function (callback) {
+      Author.findById(req.body.authorid).exec(callback)
+    },
+    authors_books: function (callback) {
+      Book.find({ 'author': req.body.authorid }).exec(callback)
+    },
+  }, function (err, results) {
+    if (err) { return next(err); }
+    // Success
+    if (results.authors_books.length > 0) {
+      // Author has books. Render in same way as for GET route.
+      res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books });
+      return;
+    }
+    else {
+      // Author has no books. Delete object and redirect to the list of authors.
+      Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
+        if (err) { return next(err); }
+        // Success - go to author list
+        res.redirect('/catalog/authors')
+      })
+    }
   });
 };
 
